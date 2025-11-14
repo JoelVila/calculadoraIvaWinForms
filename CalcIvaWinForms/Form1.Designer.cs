bd1b26cf8b0ab606668e40c501ed146251debf38
Joel Vila <vila.joel2004@gmail.com>
2025-11-14 16:42:17 +0100

    proyecto iva

---
+++ CalcIvaWinForms/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace CalcIvaWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBox_cantidadProductos = new System.Windows.Forms.TextBox();
+            this.txtBox_precioUnidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingresa el precio por unidad:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ingresa la cantidad de productos:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(310, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(344, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Calculadora IVA";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtBox_cantidadProductos
+            // 
+            this.txtBox_cantidadProductos.Location = new System.Drawing.Point(225, 134);
+            this.txtBox_cantidadProductos.Name = "txtBox_cantidadProductos";
+            this.txtBox_cantidadProductos.Size = new System.Drawing.Size(100, 20);
+            this.txtBox_cantidadProductos.TabIndex = 6;
+            this.txtBox_cantidadProductos.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtBox_precioUnidad
+            // 
+            this.txtBox_precioUnidad.Location = new System.Drawing.Point(201, 96);
+            this.txtBox_precioUnidad.Name = "txtBox_precioUnidad";
+            this.txtBox_precioUnidad.Size = new System.Drawing.Size(100, 20);
+            this.txtBox_precioUnidad.TabIndex = 7;
+            this.txtBox_precioUnidad.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Selecciona el tipo de IVA(4,10,21)";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0%",
+            "4%",
+            "10%",
+            "21%"});
+            this.comboBox1.Location = new System.Drawing.Point(238, 176);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(425, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(177, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Ingresa el procentaje de descuento:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(628, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(425, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Marcar si eres socio";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(628, 140);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(60, 293);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "NO IVA:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(60, 326);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "CON IVA:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(141, 290);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 16;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(141, 323);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBox_precioUnidad);
+            this.Controls.Add(this.txtBox_cantidadProductos);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBox_cantidadProductos;
+        private System.Windows.Forms.TextBox txtBox_precioUnidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ CalcIvaWinForms/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CalcIvaWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Gray;
+            // Llama al método de inicialización del ComboBox
+            Form1_Load(null, null);
+        }
+
+        // Inicializa el ComboBox con las tasas de IVA al cargar el formulario
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Asumiendo que el ComboBox se llama comboBox1
+            comboBox1.Items.Add("4");
+            comboBox1.Items.Add("10");
+            comboBox1.Items.Add("21");
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 2; // Selecciona 21 por defecto
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal precioUnidad;
+                int cantidad;
+                decimal tipoIVA;
+
+                // Inicialización de descuentoPorcentaje a 0m
+                decimal descuentoPorcentaje = 0m;
+
+                // --- 1️⃣ Validar y convertir cada campo ---
+
+                // Precio por unidad (txtBox_precioUnidad)
+                if (!decimal.TryParse(txtBox_precioUnidad.Text, out precioUnidad) || precioUnidad < 0)
+                    throw new Exception("Por favor ingresa un precio válido en 'Precio por unidad'.");
+
+                // Cantidad de productos (txtBox_cantidadProductos)
+                if (!int.TryParse(txtBox_cantidadProductos.Text, out cantidad) || cantidad < 0)
+                    throw new Exception("Por favor ingresa una cantidad válida en 'Cantidad de productos'.");
+
+                // Descuento (%) - CORREGIDO PARA MANEJAR CAMPOS VACÍOS (asumiendo textBox3)
+                string descuentoTexto = textBox3.Text;
+                if (!string.IsNullOrWhiteSpace(descuentoTexto))
+                {
+                    if (!decimal.TryParse(descuentoTexto, out descuentoPorcentaje))
+                    {
+                        throw new Exception("El porcentaje de descuento no es válido (ingresa solo números).");
+                    }
+                }
+
+                // Validación de rango (tanto si es 0 como si fue parseado)
+                if (descuentoPorcentaje < 0 || descuentoPorcentaje > 100)
+                {
+                    throw new Exception("El porcentaje de descuento no es válido (debe ser 0-100).");
+                }
+
+                // Tipo de IVA (comboBox1)
+                if (comboBox1.SelectedItem == null || !decimal.TryParse(comboBox1.SelectedItem.ToString(), out tipoIVA) || tipoIVA < 0)
+                    throw new Exception("Selecciona un tipo de IVA válido.");
+
+                bool esSocio = checkBox1.Checked;
+                const decimal descuentoSocio = 0.05m;
+
+                // --- 2️⃣ Calcular totales ---
+                decimal subtotalBruto = precioUnidad * cantidad;
+                decimal montoDescuentoBase = subtotalBruto * (descuentoPorcentaje / 100m);
+                decimal subtotalDescontadoBase = subtotalBruto - montoDescuentoBase;
+                decimal montoDescuentoSocio = esSocio ? subtotalDescontadoBase * descuentoSocio : 0m;
+
+                decimal totalSinIVA = subtotalDescontadoBase - montoDescuentoSocio;
+
+                decimal factorIVA = tipoIVA / 100m;
+                decimal totalConIVA = totalSinIVA * (1m + factorIVA);
+
+
+                // --- 3️⃣ Asignación de Resultados (usando textBox4 y textBox5) ---
+
+                // NO IVA
+                textBox4.Text = totalSinIVA.ToString("0.00") + " €";
+
+                // CON IVA
+                textBox5.Text = totalConIVA.ToString("0.00") + " €";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error de Cálculo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Limpiar los campos de resultado en caso de error
+                textBox4.Text = "";
+                textBox5.Text = "";
+            }
+        }
+        // Se dejan aquí para evitar que el diseñador de Visual Studio se rompa.
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label3_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void label5_Click(object sender, EventArgs e) { }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Este método ahora está vacío ya que la lógica principal está en button1_Click
+        }
+
+        private void label2_Click_1(object sender, EventArgs e) { }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e) { }
+        private void label6_Click(object sender, EventArgs e) { }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Este método ahora está vacío ya que la lógica principal está en button1_Click
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) { }
+        private void label8_Click(object sender, EventArgs e) { }
+        private void textBox4_TextChanged(object sender, EventArgs e) { }
+        private void textBox5_TextChanged(object sender, EventArgs e) { }
+    }
+}
